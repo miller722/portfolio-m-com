ac8550d996fc3bad315c2e8c3d295af0ad0e923e
miller722 <miller722@users.noreply.github.com>
2023-11-22 12:21:56 +0000

    deploy: 82971d0d9ef4864942f91c962e8650fe2ae54c9f

---
+++ assets/MyContacts-2e7ebd72.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-89dec874.js";const r=()=>t.jsx("div",{children:"MyContact"});export{r as default};
---
+++ assets/Resume-8282b274.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-89dec874.js";const r=()=>e.jsx("div",{children:"Resume"});export{r as default};
