c688d93f765dd9b29256ccf9acf3e8b836d1601b
miller <18mishak2000@gmail.com>
2023-12-03 17:43:03 +0200

    add swiper v2.0

--- src/components/Tabs/Portfolio.tsx
+++ src/components/Tabs/Portfolio.tsx
@@ -11,31 +11,33 @@ import { EffectCoverflow, Pagination } from 'swiper/modules';
 const Portfolio = () => {
     return (
         <>
-            <h2 className="project__heading">PORTFOLIO</h2>
-            <Swiper
-                effect={'coverflow'}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={'auto'}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 100,
-                    modifier: 1,
-                    slideShadows: false,
-                }}
-                pagination={false}
-                modules={[EffectCoverflow, Pagination]}
-                className="mySwiper"
-            >
-                {projectDataList.map((item, index) => (
-                    <SwiperSlide key={index}>
-                        <img src={`/img/${item.src}`} onClick={() => window.open(item.path, "_blank")} />
-                        <p className='project__description'>{item.description}</p>
-                    </SwiperSlide>
-                ))}
+            <div className='projects__wrapper'>
+                <h2 className="projects__heading">PORTFOLIO</h2>
+                <Swiper
+                    effect={'coverflow'}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={'auto'}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 100,
+                        modifier: 1,
+                        slideShadows: false,
+                    }}
+                    pagination={false}
+                    modules={[EffectCoverflow, Pagination]}
+                    className="mySwiper"
+                >
+                    {projectDataList.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <img src={`/img/${item.src}`} onClick={() => window.open(item.path, "_blank")} />
+                            <p className='projects__description'>{item.description}</p>
+                        </SwiperSlide>
+                    ))}
 
-            </Swiper>
+                </Swiper>
+            </div>
         </>
     );
 }
