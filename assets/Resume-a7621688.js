e6f05f7932fe056bd82bd8509dc20ca356401705
miller722 <miller722@users.noreply.github.com>
2024-04-13 10:38:08 +0000

    deploy: b45e41a9aa69fa8158f8c6de3146894919dd1160

--- assets/Resume-a7621688.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./index-18784e7e.js";const o="/assets/MykhailosResume-58078049.pdf",r=()=>(e.useEffect(()=>{(()=>{window.open(o,"_blank")})()},[]),s.jsx("div",{}));export{r as default};
