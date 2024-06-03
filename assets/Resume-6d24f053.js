08cb2ecf828a3be5caf553d5e77f0d7401c49da5
miller722 <miller722@users.noreply.github.com>
2024-06-03 12:58:53 +0000

    deploy: fcee052d52ea2c836e10af54fb2f67b739c4a512

---
+++ assets/Resume-6d24f053.js
@@ -0,0 +1,1 @@
+import{r as e,j as s}from"./index-8fbea9f3.js";const o="/assets/MykhailosResume-58078049.pdf",r=()=>(e.useEffect(()=>{(()=>{window.open(o,"_blank")})()},[]),s.jsx("div",{}));export{r as default};
--- assets/Resume-e2ace374.js
+++
@@ -1,1 +0,0 @@
-import{r as e,j as s}from"./index-ba7752a4.js";const o="/assets/MykhailosResume-58078049.pdf",r=()=>(e.useEffect(()=>{(()=>{window.open(o,"_blank")})()},[]),s.jsx("div",{}));export{r as default};
