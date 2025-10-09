848a8a275b260c0424d8e38a6c92d6b2a77df96a
Міша Комнацький <18miller2000@gmail.com>
2025-10-09 13:38:35 +0300

    Updates

---
+++ assets/Resume-e1a2189d.js
@@ -0,0 +1,1 @@
+import{r as e,j as s}from"./index-4df4a1e4.js";const o="/assets/MykhailosResume-58078049.pdf",r=()=>(e.useEffect(()=>{(()=>{window.open(o,"_blank")})()},[]),s.jsx("div",{}));export{r as default};
